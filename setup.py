3dd7faccff59518ec6fafab5f10287cc24cb599d
dkg0101 <dkgurav0101@gmail.com>
2024-03-19 19:49:59 +0530

    requirements updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.1"
-REPO_NAME = "MongoDB_Connector"
+REPO_NAME = "DPP_MLOPs"
 PKG_NAME= "DPP_MLOPs"
 AUTHOR_USER_NAME = "dkg0101"
 AUTHOR_EMAIL = "dkgurav0101@gmail.com"
@@ -29,14 +29,15 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A python package for predicting DIAMOND Prices.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "src"},
     packages=find_packages(where="src"),
+    package_dir={"": "src"},
+
     install_requires = get_requirements('./requirements_dev.txt')
     )
